7deabd289d81d6c891b62a41f5cbc6a5ceea8ad1
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2023-02-13 18:18:35 +0700

    feat: add basic config for main.go file

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/dzikriqalampacil/cariilmu-course/app"
 	"github.com/dzikriqalampacil/cariilmu-course/controller"
@@ -9,12 +11,23 @@ import (
 	"github.com/dzikriqalampacil/cariilmu-course/repository"
 	"github.com/dzikriqalampacil/cariilmu-course/service"
 	"github.com/go-playground/validator/v10"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	errEnv := godotenv.Load("../.env")
+	if errEnv != nil {
+		log.Fatalf("Some error occured. Err: %s", errEnv)
+	}
 
-	db := app.NewDB()
+	host := os.Getenv("host")
+	port := os.Getenv("port")
+	user := os.Getenv("user")
+	password := os.Getenv("password")
+	dbname := os.Getenv("dbname")
+	db := app.NewDB(host, port, user, password, dbname)
+
 	validate := validator.New()
 	userRepository := repository.NewUserRepository()
 	userService := service.NewUserService(userRepository, db, validate)
